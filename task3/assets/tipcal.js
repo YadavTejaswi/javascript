ce3e0f4fc699c06904c6a8c8f651d0f39605b9ab
YadavTejaswi <tejaswi.yadav@successive.tech>
2021-11-12 10:38:03 +0530

    third

---
+++ task3/assets/tipcal.js
@@ -0,0 +1,29 @@
+function tcalc(){
+    var price = document.getElementById("taxprice").value,
+    price = parseInt(price);
+
+
+    if(price<=100){
+        var tax = (price * 15) / 100;
+
+        netAmount = price + tax;
+        console.log(" tax " +tax);  
+        console.log(" total " +netAmount);
+        document.getElementById("taxamt").value = tax;
+    document.getElementById("taxgrand").value = netAmount;
+  
+
+    }else{
+        var tax = (price * 20) / 100;
+
+        netAmount = price + tax;
+        console.log(" tax " +tax);  
+        console.log(" total " +netAmount);
+        document.getElementById("taxamt").value = tax;
+    document.getElementById("taxgrand").value = netAmount;
+  
+    }
+
+   return false;
+    
+  }
